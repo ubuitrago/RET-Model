34c484d80f494316b4ba0014aa6faf69dc561574
AndrewDuffy1 <48139314+AndrewDuffy1@users.noreply.github.com>
2019-03-26 20:30:33 -0500

    Add files via upload

--- BeamT.m
+++ BeamT.m
@@ -6,7 +6,7 @@ if N < 80 || N > 354
     ro = 1.03;
     r1 = 1.01;
     rk = 1;
-elseif (171 < N)&& (N < 260)
+elseif (171 <= N)&& (N < 260)
     ro = .97;
     r1 = .99;
     rk = 1.02;
@@ -25,5 +25,18 @@ ao = ro*aoo;
 a1 = r1*a1o;
 k = rk*ko;
 
-value = ao + a1*exp(-k/cosd(90-SolarAltitude));
+if length(SolarAltitude)>1
     
+    beamt = 0;
+
+    for i = 1:length(SolarAltitude)
+        if SolarAltitude(i)>0
+            beamt(i) = ao + a1*exp(-k/cosd(90-SolarAltitude(i)));
+        else
+            beamt(i) = 0;
+        end
+    end
+    value = beamt;
+else
+    value = ao + a1*exp(-k/cosd(90-SolarAltitude));
+end
--- TestScript.m
+++ TestScript.m
@@ -27,3 +27,19 @@ Icbd = Icbd(Icb(Io,BeamT,Incident),Icd(DiffuseT,Io,SolarAltitude,Tilt)); %clear 
 
 P = Power(Icbd,Efficiency,T,length,width,n);
 
+%%
+clear variables
+N=171; %day of the year
+Long = 97.7528; %Longitude of the Event Center
+LongStand = 90;
+L = 30.2607; %Latitude of the Event Center
+t = 12.00; %Decimal Local Time (noon)
+Tilt = 22; %degrees
+PanelAzimuth = 46; %degrees
+A = .149047; %altitude in km
+Efficiency = .157; %decimal efficiency
+T = 25; %panel temp in celsius
+length = 1.64; %meters
+width = .99; %meters
+n = 960; %panels
+dailypower(N,A,Long,LongStand,L,Tilt,PanelAzimuth,Efficiency,T,length,width,n,0,24)
