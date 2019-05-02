f0ba8f419b362ae8cf132331b403b708a4c58c5c
Kazi <37486852+slkazi@users.noreply.github.com>
2019-05-02 18:51:45 -0500

    Add files via upload
    celsius no fahrenheit

--- T_infinity.m
+++ T_infinity.m
@@ -1,12 +1,16 @@
 %input time and day
 function value = T_infinity(clockTime,day)
 
-All_T_max=[61.5 65.2 72.2 79.8 86.5 92.1 95.6 97 90.5 81.8 71.4 62.7];    %T max for every month
-All_T_min=[41.5 44.8 51.3 58.6 66.7 72.3 74.4 74.6 69.4 60.6 50.6 42.3];  %T min for every month
+All_T_max=[61.5 65.2 72.2 79.8 86.5 92.1 95.6 97 90.5 81.8 71.4 62.7];    %T max for every month in F
+All_T_min=[41.5 44.8 51.3 58.6 66.7 72.3 74.4 74.6 69.4 60.6 50.6 42.3];  %T min for every month in F
 month=findMonth(day); %find which month it is
 
 T_max=All_T_max(month); %t max for current month
 T_min=All_T_min(month);%t min for current month
+
+%Converting to celsius
+T_max=5/9*(T_max-32);
+T_min=5/9*(T_min-32);
 
 %This section is for graphing
 time=linspace(0,24,1000); %x axis
