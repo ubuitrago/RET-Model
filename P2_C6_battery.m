73f03dc4adbb1f81c7be006b7c51702521b737ac
AndrewDuffy1 <Andrewduffy@utexas.edu>
2019-05-05 18:14:55 -0500

    fixes

--- P2_C6_battery.m
+++ P2_C6_battery.m
@@ -29,7 +29,7 @@ for i = 1:length(TOD)
     % next we either store battery power, use battery power, or buy energy     
     if storage(i) == capacity && Ptot(i)>iconsumption(i)
         esold = esold + (Ptot(i)-iconsumption(i))*5/60; %if the batteries are full, excess energy is sold
-    elseif Ptot(i)>iconsumption(i) && storage <capacity %checks if there is excess power and battery room
+    elseif Ptot(i)>iconsumption(i) && storage(i) <capacity %checks if there is excess power and battery room
         if i >1
             storage(i) = storage(i-1) + (Ptot(i)-iconsumption(i))*5/60; %adds to storage. times 5/60 for the 5 minute step
             if storage(i) > capacity %if the step that was just executed excedes the capacity
@@ -45,7 +45,7 @@ for i = 1:length(TOD)
         end
     elseif Ptot(i) <= iconsumption(i) %if we don't have enough solar power
         if iconsumption(i)*5/60 > storage+Ptot(i)*5/60 %not enough battery storage
-            cconsumption(i) = (iconsumption(i)*5/60-storage(i)); %some energy is purchased
+            cconsumption(i) = (iconsumption(i)*5/60-storage(i)-Ptot(i)*5/60); %some energy is purchased
             storage(i) = 0; %all storage is depleted to decrease purchased amount
         elseif iconsumption(i)*5/60 <storage(i) + Ptot(i)*5/60
             cconsumption(i) = 0;
