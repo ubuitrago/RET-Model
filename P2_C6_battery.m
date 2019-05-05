5dae125ed3225b8b29269749a719660b69dff254
AndrewDuffy1 <Andrewduffy@utexas.edu>
2019-05-05 17:05:42 -0500

    added esold to the battery model

--- P2_C6_battery.m
+++ P2_C6_battery.m
@@ -2,17 +2,14 @@
 %the number of batteries.
 global TOD;
 N = 172;
-Ptot;
+load("DailyOCI.mat");
+Ptot = P2_C6(N,index_OCI(N),25,46,960)/1000;
 Batteries = 0; %0,6, or 24
 cconsumption = 0; % cummulative consumption in kWh that had to be purchased from Austin Energy
 iconsumption = zeros(1,length(TOD)); %instantaneous consumption kW
 storage = zeros(1,length(TOD)); %kWh
-
-
-
-
-
-Capacity = Batteries*210; % 210 kWh per battery
+esold = 0; %energy sold
+capacity = Batteries*210; % 210 kWh per battery
 for i = 1:length(TOD)
     if N >= 152 && N <=324 %summer days , June 1 to nov 20
         if TOD(i) <= 6 || TOD(i) >19 %midnight to 6am or 7pm to midnight
@@ -29,13 +26,24 @@ for i = 1:length(TOD)
     
     end
     %we now have instantaneous demand for this time of day
-    % next we either store battery power, use battery power, or buy energy        
-    if Ptot(i)>iconsumption(i) && storage <capacity %checks if there is excess power and battery room
-        storage(i) = storage(i) + (Ptot(i)-iconsumption(i))*5/60; %adds to storage. times 5/60 for the 5 minute step
-        if storage(i) > capacity %if the step that was just executed excedes the capacity
-            storage(i) = capacity; %reduce to full capacity
+    % next we either store battery power, use battery power, or buy energy     
+    if storage(i) == capacity && Ptot(i)>iconsumption(i)
+        esold = esold + (Ptot(i)-iconsumption(i))*5/60; %if the batteries are full, excess energy is sold
+    elseif Ptot(i)>iconsumption(i) && storage <capacity %checks if there is excess power and battery room
+        if i >1
+            storage(i) = storage(i-1) + (Ptot(i)-iconsumption(i))*5/60; %adds to storage. times 5/60 for the 5 minute step
+            if storage(i) > capacity %if the step that was just executed excedes the capacity
+                esold = esold + (storage(i)-capacity); %sell the excess
+                storage(i) = capacity; %reduce to full capacity
+            end
+        else
+            storage(i) = 0 + (Ptot(i)-iconsumption(i))*5/60; %adds to storage. times 5/60 for the 5 minute step
+            if storage(i) > capacity %if the step that was just executed excedes the capacity
+                esold = esold + (storage(i)-capacity); %sell the excess
+                storage(i) = capacity; %reduce to full capacity
+            end
         end
-    elseif Ptot(i) < iconsumption(i) %if we don't have enough solar power
+    elseif Ptot(i) <= iconsumption(i) %if we don't have enough solar power
         if storage(i) >= iconsumption(i)*5/60 %if there is enough stored to meet full need
             storage(i) = storage(i-1) - iconsumption(i)*5/60; %demand is met by battery
         elseif iconsumption(i)*5/60 > storage %not enough battery storage
@@ -43,6 +51,7 @@ for i = 1:length(TOD)
             storage(i) = 0; %all storage is depleted to decrease purchased amount
         end
     end
+    
 end
 
     
